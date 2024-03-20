2973f42347ece115842f6e4595c4e7fffcd15aa4
ves-sev <newsecretword@yandex.ru>
2024-03-20 18:06:13 +0300

    routeConfig added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,8 +3,7 @@ import {Link, Route, Routes} from 'react-router-dom';
 import './styles/index.scss';
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import { AppRouter } from './providers/router/ui/AppRouter';
 
 
 const App = () => {
@@ -15,12 +14,7 @@ const App = () => {
             <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage />}/>
-                    <Route path={'/'} element={<MainPage />}/>
-                </Routes>
-            </Suspense>
+           <AppRouter/>
         </div>
     );
 };
