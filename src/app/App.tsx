447887837901963beab418291603b66f409a7d56
ves-sev <newsecretword@yandex.ru>
2024-03-22 20:46:28 +0300

    Navbar,AppLink added, inverted themes added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,16 @@
-import React, {Suspense} from 'react';
-import {Link, Route, Routes} from 'react-router-dom';
 import './styles/index.scss';
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import { AppRouter } from './providers/router/ui/AppRouter';
-
+import { Navbar } from 'widgets/Navbar';
 
 const App = () => {
    const { theme, toggleTheme } = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
+            <Navbar/>
             <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
            <AppRouter/>
         </div>
     );
---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,29 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import cls from "./AppLink.module.scss";
+import { Link, LinkProps } from "react-router-dom";
+import { FC } from "react";
+
+export enum AppLinkTheme{
+    PRIMARY='primary',
+    SECONDAARY='secondary'
+}
+
+interface AppLinkProps extends LinkProps {
+  className?: string;
+  theme?:AppLinkTheme;
+}
+
+export const AppLink: FC<AppLinkProps> = (props) => {
+  const { to, className, children, theme=AppLinkTheme.PRIMARY,...otherProps } = props;
+  console.log('cls.AppLink',cls.AppLink)
+  return (
+    <Link
+      to={to}
+      className={classNames(cls.AppLink, {}, [className,cls[theme]])}
+      {...otherProps}
+    >
+      {children}
+    </Link>
+  );
+};
+
