4cbdd49271ebbe0fd1fad05b28f948fc68f2eb5a
ves-sev <newsecretword@yandex.ru>
2024-05-15 17:20:13 +0300

    added a condition to buildPlugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -5,7 +5,7 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 export function buildPlugins ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
+  const plugins = [
     new HtmlWebpackPlugin({
       template: paths.html
     }),
@@ -17,7 +17,11 @@ export function buildPlugins ({ paths, isDev }: BuildOptions): webpack.WebpackPl
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev)
     }),
-    new ReactRefreshWebpackPlugin(),
-    new BundleAnalyzerPlugin({ openAnalyzer: false })
+    new ReactRefreshWebpackPlugin()
   ]
+  if (isDev) {
+    plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }))
+    plugins.push(new webpack.HotModuleReplacementPlugin())
+  }
+  return plugins
 }
