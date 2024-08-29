b37f67c058fa946fcb80f3651d334ce73e4f0232
ves-sev <newsecretword@yandex.ru>
2024-08-29 23:55:10 +0300

    Changes have been made to the storybook webpack.config

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -12,7 +12,7 @@ export default ({ config }: { config: webpack.Configuration }) => {
     src: path.resolve(__dirname, '..', '..', 'src')
 
   }
-// @ts-ignore
+  // @ts-ignore
   config!.module!.push(paths.src)
   config!.resolve!.extensions!.push('ts', 'tsx')
   // @ts-ignore
