90d296a2cb4ec7830423f023219c2b2cb08c6d2c
ves-sev <newsecretword@yandex.ru>
2024-09-16 18:37:51 +0300

    peach theme has added

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -8,9 +8,19 @@ interface UseThemeResult {
 
 export function useTheme (): UseThemeResult {
   const { theme, setTheme } = useContext(ThemeContext)
-
+  let newTheme: Theme
   const toggleTheme = () => {
-    const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK
+    switch (theme) {
+      case Theme.DARK:
+        newTheme = Theme.LIGHT
+        break
+      case Theme.LIGHT:
+        newTheme = Theme.PEACH
+        break
+      case Theme.PEACH:
+        newTheme = Theme.DARK
+        break
+    }
     setTheme?.(newTheme)
     localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme)
   }
