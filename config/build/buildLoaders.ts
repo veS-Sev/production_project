7e74c2c1e5215434eb9a6f523cf4c884271050e8
ves-sev <newsecretword@yandex.ru>
2024-09-21 23:29:42 +0300

    refresh react plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,12 +1,13 @@
 import type webpack from 'webpack'
 import { type BuildOptions } from './types/config'
 import { buildCssLoaders } from './loaders/buildCssLoaders'
-
-export function buildLoaders ({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+import { buildBabelLoader } from './loaders/buildBabelLoader'
+export function buildLoaders (options: BuildOptions): webpack.RuleSetRule[] {
   const svgLoader = {
     test: /\.svg$/,
     use: ['@svgr/webpack']
   }
+  const { isDev } = options
   const cssLoader = buildCssLoaders(isDev)
 
   // Если не используем тайпскрипт - нужен babel-loader
@@ -24,24 +25,7 @@ export function buildLoaders ({ isDev }: BuildOptions): webpack.RuleSetRule[] {
       }
     ]
   }
-  const babelLoader =
-            {
-              test: /\.m?(js|jsx|ts|tsx)$/,
-              exclude: /node_modules/,
-              use: {
-                loader: 'babel-loader',
-                options: {
-                  presets: ['@babel/preset-env'],
-                  plugins: [
-                    ['i18next-extract',
-                      {
-                        locales: ['ru', 'en'],
-                        keyAsDefaultValue: true
-                      }]
-                  ]
-                }
-              }
-            }
+  const babelLoader = buildBabelLoader(options)
 
   return [
     fileLoader,
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -23,6 +23,7 @@ export function buildPlugins ({ paths, isDev, apiUrl, project }: BuildOptions): 
   ]
 
   if (isDev) {
+    plugins.push(new ReactRefreshWebpackPlugin())
     plugins.push(new webpack.HotModuleReplacementPlugin())
     plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }))
   }
