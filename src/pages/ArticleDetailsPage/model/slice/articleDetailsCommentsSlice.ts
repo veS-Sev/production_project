4dd2c54217f4be01730c6c9cc7521741d31e2f77
ves-sev <newsecretword@yandex.ru>
2024-12-21 00:56:54 +0300

    articleDetailsCommentsSlice

---
+++ src/pages/ArticleDetailsPage/model/slice/articleDetailsCommentsSlice.ts
@@ -0,0 +1,49 @@
+import {
+  createSlice,
+  createEntityAdapter,
+  type PayloadAction
+} from '@reduxjs/toolkit'
+import { type StateSchema } from 'app/providers/StoreProvider'
+import { type Comment } from 'entities/Comment'
+import { type ArticleDetailsCommentsSchema } from '../types/articleDetailsPageComments'
+import { fetchCommentsByArticleId } from '../services/fetchCommentsByArticleId/fetchCommentsByArticleId'
+export const commentsAdapter = createEntityAdapter<Comment>({
+  selectId: (comment) => comment.id
+})
+
+export const getArticleComments = commentsAdapter.getSelectors<StateSchema>(
+  (state) =>
+    state.articleDetailsPage?.comments || commentsAdapter.getInitialState()
+)
+
+export const articleDetailsCommentsSlice = createSlice({
+  name: 'articleDetailsCommentsSlice',
+  initialState: commentsAdapter.getInitialState<ArticleDetailsCommentsSchema>({
+    isLoading: false,
+    error: undefined,
+    ids: [],
+    entities: {
+    }
+  }),
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCommentsByArticleId.pending, (state) => {
+        state.error = undefined
+        state.isLoading = true
+      })
+      .addCase(
+        fetchCommentsByArticleId.fulfilled,
+        (state, action: PayloadAction<Comment[]>) => {
+          state.isLoading = false
+          commentsAdapter.setAll(state, action.payload)
+        }
+      )
+      .addCase(fetchCommentsByArticleId.rejected, (state, action) => {
+        state.isLoading = false
+        state.error = action.payload
+      })
+  }
+})
+
+export const { reducer: articleDetailsCommentsReducer } = articleDetailsCommentsSlice
