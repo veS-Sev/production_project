47ab6bb5ee3bf37f58ff4cf9291b52191b5022c5
ves-sev <newsecretword@yandex.ru>
2025-01-20 13:47:08 +0300

    virtualization for ArticleList added

--- src/entities/Article/ui/ArticleList/ArticleList.tsx
+++ src/entities/Article/ui/ArticleList/ArticleList.tsx
@@ -1,10 +1,16 @@
 import { classNames } from 'shared/lib/classNames/classNames'
+import {
+  List,
+  WindowScroller,
+  type ListRowProps
+} from 'react-virtualized'
 import { type HTMLAttributeAnchorTarget, memo } from 'react'
 import cls from './ArticleList.module.scss'
 import { type Article, ArticleView } from '../../model/types/article'
 import { ArticleListItem } from '../ArticleListItem/ArticleListItem'
 import { ArticleListItemSkeleton } from '../ArticleListItemSkeleton/ArticleListItemSkeleton'
 import { Text, TextSize } from 'shared/ui/Text'
+import { PAGE_ID } from 'widgets/Page'
 interface ArticleListProps {
   className?: string
   articles: Article[]
@@ -14,26 +20,84 @@ interface ArticleListProps {
 }
 
 const getSceletons = (view: ArticleView) => {
-  return new Array(view === ArticleView.BIG ? 3 : 9).fill(0).map((item, index) => (
-    <ArticleListItemSkeleton view={view} key={index}/>
-  ))
+  return new Array(view === ArticleView.BIG ? 3 : 9)
+    .fill(0)
+    .map((item, index) => <ArticleListItemSkeleton view={view} key={index} />)
 }
 
 export const ArticleList = memo((props: ArticleListProps) => {
-  const { className, view = ArticleView.SMALL, articles, isLoading, target } = props
+  const {
+    className,
+    view = ArticleView.SMALL,
+    articles,
+    isLoading,
+    target
+  } = props
+
+  const isBig = view === ArticleView.BIG
+  const itemsPerRow = isBig ? 1 : 3
+  const rowCount = isBig
+    ? articles.length
+    : Math.ceil(articles.length / itemsPerRow)
 
   if (!isLoading && articles.length === 0) {
-    return (<Text title={'Статьи не найдены'} size={TextSize.L}/>)
+    return <Text title={'Статьи не найдены'} size={TextSize.L} />
   }
-  const renderArticles = (articles: Article[]) =>
-    articles.map((article, id) => (
-      <ArticleListItem target = {target}className={cls.card} key={article.id} view={view} article={article} />
-    ))
+
+  const rowRender = ({ index, isScrolling, key, style }: ListRowProps) => {
+    console.log('@scroll Articles list')
+    const items = []
+    const fromIndex = index * itemsPerRow
+    // константа с индексом, которым ограничиваемся
+    const toIndex = Math.min(fromIndex + itemsPerRow, articles.length)
+
+    for (let i = fromIndex; i < toIndex; i += 1) {
+      items.push(
+        <ArticleListItem
+          target={target}
+          className={cls.card}
+          key={articles[i]?.id}
+          view={view}
+          article={articles[i]}
+        />
+      )
+    }
+
+    return <div key={key} style={style} className={cls.row}>{items}</div>
+  }
+
   return (
-    <div className={classNames(cls.ArticleList, {}, [cls[view], className])}>
-      {articles.length > 0 ? renderArticles(articles) : null}
-      {isLoading && getSceletons(view)}
-    </div>
+    <WindowScroller
+      onScroll={() => console.log('@scroll')}
+      scrollElement={document.getElementById(PAGE_ID) as Element}
+    >
+      {({
+        height,
+        width,
+        registerChild,
+        onChildScroll,
+        isScrolling,
+        scrollTop
+      }) => (
+        <div
+          className={classNames(cls.ArticleList, {}, [cls[view], className])}
+          ref={registerChild}
+        >
+          <List
+            autoHeight
+            autoWidth = {isBig}
+            height={height ?? 700}
+            rowCount={rowCount}
+            rowHeight={isBig ? 700 : 330 }
+            rowRenderer={rowRender}
+            width={width}
+            onScroll={onChildScroll}
+            isScrolling={isScrolling}
+            scrollTop={scrollTop}
+          />
+          {isLoading && getSceletons(view)}
+        </div>
+      )}
+    </WindowScroller>
   )
-}
-)
+})
--- src/widgets/Page/Page.tsx
+++ src/widgets/Page/Page.tsx
@@ -21,6 +21,8 @@ interface PageProps {
   onScrollEnd?: () => void
 }
 
+export const PAGE_ID = 'PAGE_ID'
+
 export const Page = (props: PageProps) => {
   const { className, children, onScrollEnd } = props
   const triggerRef = useRef() as MutableRefObject<HTMLDivElement>
@@ -54,6 +56,7 @@ export const Page = (props: PageProps) => {
       ref={wrapperRef}
       className={classNames(cls.Page, {}, [className])}
       onScroll={onScroll}
+      id = {PAGE_ID}
     >
       {children}
       {onScrollEnd && <div ref={triggerRef} className={cls.scrollEnd}/>}
