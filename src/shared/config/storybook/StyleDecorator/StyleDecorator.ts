d3e28967940a9a5d33760a3504586a0a604bc970
ves-sev <newsecretword@yandex.ru>
2025-02-27 18:05:00 +0300

    storybook has been updated from version 6 to 8

--- src/shared/config/storybook/StyleDecorator/StyleDecorator.ts
+++ src/shared/config/storybook/StyleDecorator/StyleDecorator.ts
@@ -1,4 +1,5 @@
-import { type Story } from '@storybook/react'
+// import { type Story } from '@storybook/react'
+import { type StoryFn } from '@storybook/react'
 import 'app/styles/index.scss'
 
-export const StyleDecorator = (story: () => Story) => story()
+export const StyleDecorator = (story: () => StoryFn) => story()
